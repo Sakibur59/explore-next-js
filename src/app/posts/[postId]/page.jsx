2ee46d05f7202e42eef63e9bf74e5bb821a2766e
Sakibur59 <sakiburrahman5978@gmail.com>
2026-04-11 13:54:18 +0600

    dynamic post data added

--- src/app/posts/[postId]/page.jsx
+++ src/app/posts/[postId]/page.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+const PostDetailsPage = async ({ params }) => {
+  const { postId } = await params;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const Posts = await res.json();
+  const {title,body} = Posts;
 
-const PostPage = () => {
-    return (
-        <div>
-            sda
-        </div>
-    );
+  return (
+    <div>
+      <h2>Show Details of Post: {postId}</h2>
+      <h1>Post Title: {title}</h1>
+      <p>Post Body: {body}</p>
+    </div>
+  );
 };
 
-export default PostPage;
+export default PostDetailsPage;
